90c0d18a96de960067a6c5c3ca52c8cfed9b480a
Mpange-Mosa <106333833+Mpange-Mosa@users.noreply.github.com>
2022-05-26 17:42:14 +0200

    Create computer.java

---
+++ computer.java
@@ -0,0 +1,40 @@
+package netComputers;
+
+public class Computer {
+	
+	//Member function to store IPAddress and value
+    String IPAddress;
+    float Value;
+    
+    //Parametrized constructor
+    public Computer(String ip,float value ){
+        this.IPAddress = ip;
+        this.Value = value;
+    }
+
+    public String toString(){
+        String string = this.IPAddress + " $ " + this.Value+"\n";
+        return string;
+    }
+
+}
+package netComputers;
+
+public class Computer {
+	
+	//Member function to store IPAddress and value
+    String IPAddress;
+    float Value;
+    
+    //Parametrized constructor
+    public Computer(String ip,float value ){
+        this.IPAddress = ip;
+        this.Value = value;
+    }
+
+    public String toString(){
+        String string = this.IPAddress + " $ " + this.Value+"\n";
+        return string;
+    }
+
+}
